0c53a6b88b8dc097d0afc29c4b6ba08f06d964e2
JayuDhengle <JayuDhengle@gmail.com>
2025-04-08 16:35:33 +0530

    Day11

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -1,63 +1,48 @@
 package base;
 
-import java.sql.Driver;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
-public class TestBase 
-{
+public class TestBase {
 	private static TestBase testBase;
 	private static WebDriver driver;
 
-	private TestBase()
-	{
+	private TestBase() {
 		String strBowser = "chrome";
-		
-		if(strBowser.equalsIgnoreCase("chrome"))
-		{
-			ChromeOptions options = new ChromeOptions();
-			options.addArguments("--incognito");			
-			driver = new ChromeDriver(options);
 
+		if (strBowser.equalsIgnoreCase("chrome")) {
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--incognito");
+			driver = new ChromeDriver(options);
+		} else if (strBowser.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
 		}
-		else if(strBowser.equalsIgnoreCase("edge"))
-		{
-			driver =  new EdgeDriver();
-		}
-		
+
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.manage().deleteAllCookies();
 	}
-	
-	
-	public static void initDriver()
-	{
-		if(testBase == null)
-		{
+
+	public static void initDriver() {
+		if (testBase == null) {
 			testBase = new TestBase();
 		}
 	}
-	
-	public static WebDriver getDriver()
-	{
+
+	public static WebDriver getDriver() {
 		return driver;
 	}
-	
-	public static void openURL(String url)
-	{
+
+	public static void openURL(String url) {
 		driver.get(url);
 	}
-	
-	public static void tearDown()
-	{
-		if(driver!= null)
-		{
+
+	public static void tearDown() {
+		if (driver != null) {
 			driver.close();
 			driver.quit();
 		}
